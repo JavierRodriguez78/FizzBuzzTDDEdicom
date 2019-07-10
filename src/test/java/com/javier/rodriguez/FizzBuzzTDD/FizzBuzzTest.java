4e3a33cc0105c9bb7ac4cd31895164b656a487f4
Xavi <xavi@geekshubs.com>
2019-07-10 14:45:02 +0200

    [BLUE] Refactor testClass to include DataProvider

--- src/test/java/com/javier/rodriguez/FizzBuzzTDD/FizzBuzzTest.java
+++ src/test/java/com/javier/rodriguez/FizzBuzzTDD/FizzBuzzTest.java
@@ -1,40 +1,72 @@
 package com.javier.rodriguez.FizzBuzzTDD;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 /**
  * Unit test for simple App.
  */
+
+@RunWith(DataProviderRunner.class)
 public class FizzBuzzTest 
 {
+	@DataProvider
+	public static Object[][] dataNumberProviderForFizz(){
+		
+		return new Object[][] {
+			{3, true},
+			{6, true},
+			{15, false},
+			{18, true}
+			};
+		}
+	
+	@DataProvider
+	public static Object[][] dataNumberProviderForBuzz(){
+		return new Object[][] {
+			{5, true},
+			{10, true},
+			{15, false},
+			{25, true}
+			
+		};
+	}
 	
 	@Test
-	public void itShoudReturnFizzIfDivisibleByThree()
+	@UseDataProvider("dataNumberProviderForFizz")
+	public void itShoudReturnFizzIfDivisibleByThree(final int input, final boolean expected)
 	{
 		//Arragnge
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		int value = 3;
+		
 		
 		//Act
-		boolean result = fizzBuzz.isFizz(value);
+		boolean result = fizzBuzz.isFizz(input);
 		
 		//Assertion
-		assertTrue(result);
+		assertEquals(expected, result);
 	}
 	
 	
 	@Test
-	public void itShouldReturnBuzzIfDivisibleByFive() {
+	@UseDataProvider("dataNumberProviderForBuzz")
+	public void itShouldReturnBuzzIfDivisibleByFive(final int input, final boolean expected) {
 		
 		//Arragnge
 				FizzBuzz fizzBuzz = new FizzBuzz();
-				int value = 5;
+				int value = input;
 				
 				//Act
 				boolean result = fizzBuzz.isBuzz(value);
 				
 				//Assertion
-				assertTrue(result);
+				assertEquals(expected, result);
 		
 	}
 }
